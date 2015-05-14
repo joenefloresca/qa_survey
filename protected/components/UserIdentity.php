6d9551b31db961d18967fc1c2f1f5c838ad6ce5e
Joene Floresca <joenefloresca@gmail.com>
2015-05-14 18:11:10 +0800

    Added Registration Module

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -17,17 +17,19 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
+            $users = Users::model()->findByAttributes(array('username'=>$this->username));
+            if($users === null)
+            {
+                $this->errorCode=self::ERROR_USERNAME_INVALID;
+            }else
+            {
+                if($users->check($this->password))
+                {
+                        $this->errorCode=self::ERROR_NONE;
+                }else{
+                        $this->errorCode=self::ERROR_PASSWORD_INVALID;
+                }
+            }
+            return !$this->errorCode;
 	}
 }
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -35,15 +35,32 @@
 	</div><!-- header -->
 
 	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				//array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'Forms', 'url'=>array('/forms')),
-				// array('label'=>'Contact', 'url'=>array('/site/contact')),
-				//array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				//array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
+		<?php
+
+	if (Yii::app()->user->isGuest) {
+              $this->widget('zii.widgets.CMenu',array(
+              'activeCssClass' => 'active',
+              'activateParents' => true,
+              'items'=>array(
+                   // array('label'=>'Home', 'url'=>array('/site/index')),
+                    array('label'=>'Forms', 'url'=>array('/forms/')),
+                    array('label'=>'Register', 'url'=>array('/users/index')),
+		    array('label'=>'Login', 'url'=>array('/site/login')),
+                            ),
+                        ));
+                    } else {
+                        $this->widget('zii.widgets.CMenu',array(
+                            'activeCssClass' => 'active',
+                            'activateParents' => true,
+                            'items'=>array(
+                            	array('label'=>'Home', 'url'=>array('/site/index')),
+                                array('label'=>'Forms', 'url'=>array('/forms/')),
+				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'))
+                            ),
+                        ));
+                    }
+
+?>
 	</div><!-- mainmenu -->
 	<?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
